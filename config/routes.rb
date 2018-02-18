f7e95ccab6bea69b4039a61e4503473c3dc35b79
quenvin <quenvint@hotmail.com>
2018-02-18 20:40:56 +0800

    added admin controller with index, show and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
+  # match 'users/:id' => 'users/show' , via: :get
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   
   devise_for :users
-  # match 'users/:id' => 'users/show' , via: :get
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root "posts#index"
-
-  resources :posts
 
   namespace :admin do
     resources :posts
