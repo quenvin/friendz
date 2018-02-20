afcd9d987b5ce6ddf4eaf1ee63b263b9d49f909d
Bernard <bernardleongqijie@gmail.com>
2018-02-20 13:25:59 +0800

    Fully functional edit and show user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, uniqueness: true
+  validates :username, presence: true
+  validates :email, uniqueness: true
+  validates :email, presence: true
+
   has_many :posts
 
 
 
-  #  validates_uniqueness_of :username
-
+    # validates_uniqueness_of :username, :case_sensitive => false
+    # validates_uniqueness_of :email, :case_sensitive => false
 
 
 
