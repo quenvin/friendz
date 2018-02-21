079e3f5c92d26a6bd9ed743527b16dd030775ac9
Bernard <bernardleongqijie@gmail.com>
2018-02-21 16:17:56 +0800

    create and edit fields

--- app/models/particular.rb
+++ app/models/particular.rb
@@ -3,11 +3,6 @@ class Particular < ApplicationRecord
     # devise :database_authenticatable, :registerable,
     #      :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, uniqueness: true
-  validates :age, presence: true
-  validates :gender, uniqueness: true
-  validates :introduction, presence: true
-
   belongs_to :user
 
 
