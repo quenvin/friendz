4a8135298222b22fb18e7754f291b17a4330f70c
quenvin <quenvint@hotmail.com>
2018-02-22 23:20:12 +0800

    added endless scrolling function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    # @posts = Post.all
+    @posts = Post.paginate(:page => params[:page], :per_page => 10)
     @like = Like.new
   end
 
